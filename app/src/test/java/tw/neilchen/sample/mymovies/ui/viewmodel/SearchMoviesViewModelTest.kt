3ddc4f3604604e7fb568efd11faed182a87ffb55
Neil Chen <neilchennc@gmail.com>
2025-07-14 09:00:47 +0000

    Add unit test for SearchMoviesViewModel

--- app/src/test/java/tw/neilchen/sample/mymovies/ui/viewmodel/SearchMoviesViewModelTest.kt
+++ app/src/test/java/tw/neilchen/sample/mymovies/ui/viewmodel/SearchMoviesViewModelTest.kt
@@ -4,31 +4,51 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.testing.asPagingSourceFactory
 import androidx.paging.testing.asSnapshot
+import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import retrofit2.HttpException
+import retrofit2.Response
 import tw.neilchen.sample.mymovies.data.FakeData
+import tw.neilchen.sample.mymovies.data.Movie
+import tw.neilchen.sample.mymovies.data.MovieList
+import tw.neilchen.sample.mymovies.data.SearchKeyword
+import tw.neilchen.sample.mymovies.network.TmdbApiService
+import tw.neilchen.sample.mymovies.paging.MoviesPagingSource
 import tw.neilchen.sample.mymovies.repository.DatabaseRepository
 import tw.neilchen.sample.mymovies.repository.MoviesRepository
 import tw.neilchen.sample.mymovies.repository.PreferencesRepository
+import java.util.Date
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class SearchMoviesViewModelTest {
 
     @MockK
     private lateinit var preferencesRepository: PreferencesRepository
+
+    @MockK
+    private lateinit var apiService: TmdbApiService
 
     @MockK(relaxed = true)
     private lateinit var moviesRepository: MoviesRepository
@@ -41,7 +61,7 @@ class SearchMoviesViewModelTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-        Dispatchers.setMain(UnconfinedTestDispatcher())
+        Dispatchers.setMain(StandardTestDispatcher())
 
         every { preferencesRepository.languageTag } returns flow { emit("") }
 
@@ -55,17 +75,16 @@ class SearchMoviesViewModelTest {
 
     @Test
     fun searchMovies_successResult() = runTest {
-        val query = "god"
         val result = listOf(FakeData.movie)
         val pager = Pager(
-            config = PagingConfig(1),
+            config = PagingConfig(pageSize = 1),
             pagingSourceFactory = result.asPagingSourceFactory()
         )
 
         every { preferencesRepository.languageTag } returns flowOf("")
-        every { moviesRepository.searchMovies(query, "") } returns pager.flow
+        every { moviesRepository.searchMovies(any(), any()) } returns pager.flow
 
-        viewModel.searchMovies(query)
+        viewModel.searchMovies("")
 
         val snapshot = viewModel.resultFlow.asSnapshot()
         assertThat(snapshot).isEqualTo(result)
@@ -73,11 +92,48 @@ class SearchMoviesViewModelTest {
 
     @Test
     fun searchMovies_networkError() = runTest {
-        // TODO
+        val pager = Pager(
+            config = PagingConfig(pageSize = 1),
+            pagingSourceFactory = { MoviesPagingSource(apiService, "123", "") }
+        )
+        val exception = HttpException(
+            Response.error<Any>(
+                500,
+                "Internal error".toResponseBody("text/plain".toMediaType())
+            )
+        )
+
+        every { preferencesRepository.languageTag } returns flowOf("")
+        coEvery { apiService.searchMovies(any(), any(), "") } throws exception
+        every { moviesRepository.searchMovies(any(), any()) } returns pager.flow
+
+        viewModel.searchMovies("")
+
+        try {
+            viewModel.resultFlow.asSnapshot()
+            throw Exception("Test should not be reached here")
+        } catch (e: HttpException) {
+            assertThat(e).isEqualTo(exception)
+        }
     }
 
     @Test
     fun searchMovies_insertSearchKeywords() = runTest {
-        // TODO
+        val keyword = "Hello World"
+
+        coEvery { databaseRepository.insertSearchKeyword(any()) } returns Unit
+
+        // FIXME: WTF cannot be mocked by MockK?
+        every { databaseRepository.getAllSearchKeywords() } returns flow {
+            emit(listOf(SearchKeyword(id = 0, keyword = keyword, addedAt = Date())))
+        }
+
+        viewModel.insertSearchKeyword(keyword)
+
+        viewModel.keywordsFlow.test {
+            assertThat(awaitItem()).isEqualTo(keyword)
+        }
+
+        coVerify { databaseRepository.insertSearchKeyword(any()) }
     }
 }
